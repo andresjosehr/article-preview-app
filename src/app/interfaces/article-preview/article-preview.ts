2db0049583be48f6b141a68f0694d7371f39d8a9
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-02-01 22:34:05 -0400

    Files fixed

--- src/app/interfaces/article-preview/article-preview.ts
+++
@@ -1,8 +0,0 @@
-export interface ArticlePreview {
-    title: String,
-    preview_content: String,
-    image: String,
-    autor: String,
-    autor_photo: String,
-    date: String
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  api   : "https://article-preview-api.herokuapp.com/api",
-  images: "https://article-preview-api.herokuapp.com/images/"
-};
